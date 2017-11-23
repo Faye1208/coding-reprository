c68c5a16504b0006cdc665a351f0dbd87a2d3bd7
Faye1208 <358658874@qq.com>
2017-11-23 12:59:56 +0800

    change the written of the connect api for decorator

--- redux-demo4/README.md
+++ redux-demo4/README.md
@@ -1,3 +1,13 @@
 > 使用装饰器的形式改写connect
 
- + 需要额外的自定义插件，终端运行npm run eject弹出个性化配置，该命令不可逆。
+ + 需要额外的自定义插件，终端运行npm run eject弹出个性化配置，该命令不可逆,根目录下多了config和script目录，还有package.json多了很多配置
+
+ + 安装支持装饰器的插件transform-decorators-legacy
+
+       $ npm install babel-plugin-transform-decorators-legacy --save-dev
+
+ + 在package.json中的"babel"配置项中添加代码
+
+        "plugins":["transform-decorators-legacy"]
+
+ + 改写connect
--- redux-demo4/src/App.js
+++ redux-demo4/src/App.js
@@ -4,6 +4,12 @@ import { connect } from 'react-redux';
 import { addGun, removeGun, addGunAsync } from './index.redux';
 import './App.css';
 
+// 使用装饰器的写法改写connect
+@connect(
+    state => ({ num: state }),
+    { addGun, removeGun, addGunAsync }
+)
+
 class App extends Component {
   render() {
     return (
@@ -24,11 +30,11 @@ class App extends Component {
   }
 }
 
-const mapStatetoProps = state => {
-    return { num: state };
-}
+// 没用装饰器写法
+// 需要state中的什么属性放到自己组件的props中
+// const mapStatetoProps = state => {return { num: state };}
+// 需要什么方法放到自己组件的props中，并且这些会自动dispatch
+// const actionCreators = { addGun, removeGun, addGunAsync };
+// App = connect(mapStatetoProps, actionCreators)(App);
 
-const actionCreator = { addGun, removeGun, addGunAsync };
-
-App = connect(mapStatetoProps, actionCreator)(App);
 export default App;
